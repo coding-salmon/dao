b5c3292b363ff18ec98ffb753ed1a527b2e03abd
ejzus88 <ejzus88@naver.com>
2024-03-28 01:05:32 +0900

    1

---
+++ src/main/java/com/doa/doa/DoaApplication.java
@@ -0,0 +1,13 @@
+package com.doa.doa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doa/doa/DoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doa.doa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
